157549ac915b1e3b0f8391d5a8073c3eb457356f
Steven Theodore <steventheodore98@gmail.com>
2020-11-10 00:01:09 +0700

    lecture 6 part 14

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin.js');
