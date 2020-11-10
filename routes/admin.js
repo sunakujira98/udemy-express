4ca03861e703eb7aa5837c6c0b4b021a567cddf7
Steven Theodore <steventheodore98@gmail.com>
2020-11-11 00:51:52 +0700

    lecture 7 part 3

--- routes/admin.js
+++ routes/admin.js
@@ -2,20 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+// /admin/add-product => GET
+router.get('/add-product', productController.getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add product', path: '/admin/add-product' });
-});
+// /admin/add-product => POST
+router.post('/add-product', productController.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
-})
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
