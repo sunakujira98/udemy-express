9be4adda04a8571bbbf1855be5c5d5e6834a714f
Steven Theodore <steventheodore98@gmail.com>
2020-11-11 01:22:04 +0700

    lecture 7 part 5

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,18 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', { pageTitle: 'Add product', path: '/admin/add-product' });
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
+
     res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
+
     res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
 }
