5d751748f6dc1a0503659293dfe2a2b7a9c85243
Steven Theodore <steventheodore98@gmail.com>
2020-11-11 00:59:41 +0700

    lecture 7 part 4

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 
+const errorController = require('./controllers/error');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -16,8 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
